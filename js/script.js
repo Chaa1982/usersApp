4ae1d30b2674d45e85627bdb10344953c1ab6928
Andrey Chaplinskiy <127024939+Chaa1982@users.noreply.github.com>
2023-03-09 17:14:30 +0200

    Добавлена функція edit
    Але if у строках з 67 по 77 дуже складний, наступним комітом спростимо

--- js/script.js
+++ js/script.js
@@ -8,47 +8,75 @@ const searchInput = document.querySelector("#search");
 
 let users = [
   //об'єкти добавлено у тесстовору режиміі, потім слід прибрати!!!!!!!!!!!!
-  { name: "Max", age: 20, city: "Lviv" },
-  { name: "Olga", age: 20, city: "Kharkiv" },
-  { name: "Mila", age: 40, city: "Odesa" },
+    { name: "Max", age: 20, city: "Lviv" },
+    { name: "Olga", age: 20, city: "Kharkiv" },
+    { name: "Mila", age: 40, city: "Odesa" },
 ];
+
+let changingUser = undefined; //змінна для коримтувача, який змінюємо
 
 renderUsers(users); //визвав у тестовому режимі, потім слід прибрати!!!!!!!!!!!!
 
 const deleteUser = (indexOfUser) => {
-  users = users.filter((el, i) => i !== indexOfUser);
-  renderUsers(users);
+    users = users.filter((el, i) => i !== indexOfUser);
+    renderUsers(users);
 };
 
+function editUser(indexOfUser) {
+    changingUser = {data: users[indexOfUser], index: indexOfUser};
+
+    createButton.textContent = "Save changes";
+
+    nameInput.value = changingUser.data.name;
+    ageInput.value = changingUser.data.age;
+    cityInput.value = changingUser.data.city;
+}
+
 function renderUsers(usersToRender) {
-  usersSection.innerHTML = "";
-  usersContent = usersToRender.map(
+    usersSection.innerHTML = "";
+    usersContent = usersToRender.map(
     (user) => `<div class="user-card">
         <p>${user.name}</p>
         <p>${user.age}</p>
         <p>${user.city}</p>
-        <button class="delete-user-button">Delete</button>        
+        <button class="delete-user-button">Delete</button>              
+        <button class="edit-user-button">Edit</button>        
     </div>`
-  );
+    );
 
-  usersContent.forEach((userLayout) => {
+    usersContent.forEach((userLayout) => {
     usersSection.innerHTML += userLayout;
-  });
+    });
 
-  const deleteButtons = [...document.querySelectorAll(".delete-user-button")];
-  deleteButtons.forEach((button, i) => {
+    const deleteButtons = [...document.querySelectorAll(".delete-user-button")];
+    deleteButtons.forEach((button, i) => {
     button.onclick = () => deleteUser(i);
-  });
+    });
+
+    const editButton = [...document.querySelectorAll(".edit-user-button")];
+    editButton.forEach((button, i) => {
+        button.onclick = () => editUser(i);
+    });
 }
 
 createButton.onclick = () => {
     const name = nameInput.value;
     const age = +ageInput.value;
     const city = cityInput.value;
-    console.log(name, age, city);
-
-    const user = { name, age, city };
+    
+    if(changingUser){
+        users = users.map((el, i) => {
+            if(i === changingUser.index){
+                return {name, age, city};
+            } else return el;
+            
+        });
+        changingUser = undefined;
+        createButton.textContent = "Create User";
+    }else{
+        const user = { name, age, city };
     users.push(user);
+    }
 
     nameInput.value = "";
     ageInput.value = "";
